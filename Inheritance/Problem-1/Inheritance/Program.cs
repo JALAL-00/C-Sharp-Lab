033a4a3a767e936f664b49972c4f8818df17265c
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-11-01 03:36:13 +0600

    Add files via upload

---
+++ Inheritance/Problem-1/Inheritance/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Human
+{
+    public string name { get; set; }
+    public int age { get; set; }
+    public Human(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public void humaninfo()
+    {
+        Console.WriteLine("Human Info");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine();
+    }
+}
+
+
+class Student : Human
+{
+    public double cg { get; set; }
+
+    public Student(string name, int age, double cg) : base(name, age)
+    {
+        this.cg = cg;
+    }
+
+    public void studentInfo()
+    {
+        Console.WriteLine("Student Info");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("CG: " + cg);
+        Console.WriteLine();
+    }
+}
+
+class ExStudent : Student
+{
+    public int graduationyear { get; set; }
+
+    public ExStudent(string name, int age, double cg, int graduationyear) : base(name, age, cg)
+    {
+        this.graduationyear = graduationyear;
+    }
+
+    public void exstudentInfo()
+    {
+        Console.WriteLine("Ex Student Info");
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("CG: " + cg);
+        Console.WriteLine("Graduation Year: " + graduationyear);
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Human h1 = new Human("JALAL", 23);
+        h1.humaninfo();
+
+        Student s2 = new Student("JALAL", 22, 3.7);
+        s2.studentInfo();
+
+        ExStudent ex = new ExStudent("JALAL", 22, 3.7, 2020);
+        ex.exstudentInfo();
+
+
+        Console.ReadKey();
+    }
+}
---
+++ Inheritance/Problem-2/Inheritance-2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Bird
+{
+    public string color { set; get; }
+    public string size { set; get; }
+
+    public Bird(string color, string size)
+    {
+        this.color = color;
+        this.size = size;
+    }
+
+    public void birdInfo()
+    {
+        Console.WriteLine("Bird Class");
+        Console.WriteLine("Color: " + color);
+        Console.WriteLine("size: " + size);
+    }
+}
+
+class Penguin : Bird
+{
+    public int lifespan { set; get; }
+
+    public Penguin(string color, string size, int lifespan) : base(color, size)
+    {
+        this.lifespan = lifespan;
+    }
+
+    public void CanFly()
+    {
+        Console.WriteLine("Penguin can not fly");
+
+    }
+}
+
+class Robin : Bird
+{
+    public int lifespan { set; get; }
+
+    public Robin(string color, string size, int lifespan) : base(color, size)
+    {
+        this.lifespan = lifespan;
+    }
+
+    public void CanFly()
+    {
+
+        Console.WriteLine("Dual can fly");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Robin r = new Robin("Black", "Medium", 13);
+        Penguin p = new Penguin("White and Black", "Big", 16);
+        p.CanFly();
+        p.birdInfo();
+        Console.WriteLine();
+        r.CanFly();
+        r.birdInfo();
+
+        Console.ReadKey();
+    }
+}
+
+
